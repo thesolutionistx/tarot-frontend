38a90dd9d8d878d13b395a90fcfbd231d20a4992
thesolutionistx <solutionistservices@gmail.com>
2025-03-21 20:10:00 -0400

    bigone

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    trailingSlash: true,
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  // Add this to help with client-side routing in static exports
+  experimental: {
+    // This will allow client-side navigation to work properly in static export
+    scrollRestoration: true,
   }
-  
-  module.exports = nextConfig
-  
+}
+
+module.exports = nextConfig
