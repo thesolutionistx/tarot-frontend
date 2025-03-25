dc77757ac35e87b3963bf2c4fa3b9248c4fa6352
thesolutionistx <solutionistservices@gmail.com>
2025-03-24 20:10:21 -0400

    third is a hope

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  distDir: '.next'
+}
+
+module.exports = nextConfig 
